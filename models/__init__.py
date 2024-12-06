c5c3d5f72ea22baded6c9ce8def6f5844da66b86
7amzawey <mohamedhamza123467890@gmail.com>
2024-12-06 16:36:21 +0200

    starting from scratch

--- models/__init__.py
+++
@@ -1,6 +0,0 @@
-from models.engine.file_storage import FileStorage
-storage = FileStorage()
-storage.reload()
-from models.base_model import BaseModel
-globals()['BaseModel'] = BaseModel
-#globals()['User'] = User
